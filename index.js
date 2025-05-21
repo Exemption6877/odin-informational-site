e49c1bfc18ce40c26e9015a2c161cb09b63d4f47
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-21 06:48:51 +0300

    Use express for page routing

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-let http = require("http");
-let url = require("url");
-let fs = require("fs");
-
 const PORT = 8080;
+const express = require("express");
+const app = express();
 
-http
-  .createServer((req, res) => {
-    let q = url.parse(req.url, true);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-    let filename =
-      q.pathname === "/" ? "./index.html" : "./" + q.pathname + ".html";
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
 
-    const path404 = "./404.html";
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        fs.readFile(path404, function (err404, data404) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          res.write(data404);
-          return res.end();
-        });
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
-  })
-  .listen(PORT);
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}.`);
+});
